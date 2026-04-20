0ee87fde8c15a6c8ebd6d1724815923e76659800
Khushi Sahu <khushisahu.1956@gmail.com>
2026-04-21 03:31:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("Welcome !");
+console.log("Code is running..");
+console.log("Code is still running");
+console.log("Code is incredible");
+
+var a = prompt("Username");
+var isTrue =confirm("sure");
+if(isTrue){
+    console.log("Pleased")
+}
+else{
+    console.log("Disappointed")
+}
+console.log("Welcome"+ a)
